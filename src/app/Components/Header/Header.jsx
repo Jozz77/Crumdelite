fe25908ac6fd22dc5fb090d678c168dce8980dea
Jozz77 <mmadujonathan@gmail.com>
2023-10-26 03:39:02 +0100

    add the header

---
+++ src/app/Components/Header/Header.jsx
@@ -0,0 +1,36 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import Logo from './Assets/Logo.png'
+
+export default function Header() {
+  return (
+    <header className=' fixed top-0 w-full flex justify-between items-center py-2 px-[5%] '>
+      <nav className='w-[15%] flex justify-between '>
+        <Link href="">
+            Home
+        </Link>
+        <Link href="">
+            Home
+        </Link>
+        <Link href="">
+            Home
+        </Link>
+      </nav>
+      <div className='w-[10%] '>
+        <Image src={Logo} className=' w-full' alt='Logo Image' />
+      </div>
+      <nav className='w-[15%] flex justify-between '>
+        <Link href="">
+            Home
+        </Link>
+        <Link href="">
+            Home
+        </Link>
+        <Link href="">
+            Home
+        </Link>
+      </nav>
+    </header>
+  )
+}
---
+++ src/app/Components/HomeHero/HomeHero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HomeHero() {
+  return (
+    <div className='Home-Hero1 h-[100vh] w-full '>
+      
+    </div>
+  )
+}
